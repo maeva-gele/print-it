84dfa4725fbc87c8eae3fab09a95d0643f871558
Maeva Gele <maeva.l.gele@gmail.com>
2026-03-02 18:11:25 +0100

    création de la balise image pour le carrousel

--- assets/script.js
+++ assets/script.js
@@ -17,3 +17,29 @@ const slides = [
 	}
 ]
 console.log(slides)
+
+const basePath = "assets/images/slideshow/"
+
+// baliseImage = document.getElementById("carrousel")
+// baliseImage.setAttribute(slides[i], )
+// console.log(baliseImage) 
+
+const boutonDroite = document.getElementById("btnDroite")
+    boutonDroite.addEventListener("click", () => {
+        console.log("le bouton droit à été cliqué")
+	})
+
+const boutonGauche = document.getElementById("btnGauche")
+    boutonGauche.addEventListener("click", () => {
+        console.log("le bouton gauche à été cliqué")
+    })
+
+const banner = document.getElementById("banner")
+const img = document.createElement("img")
+img.classList.add("banner-img")
+let index=0
+
+img.src= basePath + slides[index].image 
+img.alt= slides[index].tagLine
+
+banner.appendChild(img)
