b92318052e0325bb71cdcf495371597223302581
Maeva Gele <maeva.l.gele@gmail.com>
2026-03-09 17:43:57 +0100

    creation du carrousel

--- assets/script.js
+++ assets/script.js
@@ -19,27 +19,42 @@ const slides = [
 console.log(slides)
 
 const basePath = "assets/images/slideshow/"
+let index = 0
 
-// baliseImage = document.getElementById("carrousel")
-// baliseImage.setAttribute(slides[i], )
-// console.log(baliseImage) 
-
-const boutonDroite = document.getElementById("btnDroite")
-    boutonDroite.addEventListener("click", () => {
-        console.log("le bouton droit à été cliqué")
-	})
-
-const boutonGauche = document.getElementById("btnGauche")
-    boutonGauche.addEventListener("click", () => {
-        console.log("le bouton gauche à été cliqué")
-    })
-
+//CREATION DES IMAGES 
 const banner = document.getElementById("banner")
 const img = document.createElement("img")
 img.classList.add("banner-img")
-let index=0
 
-img.src= basePath + slides[index].image 
-img.alt= slides[index].tagLine
+//CREATION DU TEXTE 
+const txt = document.createElement("p")
 
+//CREATION DES DOTS 
+const dotsHome = document.getElementsByClassName("dots")
+const dot = document.createElement("div")
+dot.classList.add("dot")
+
+//VARIABLES DU CAROUSSEL 
+function caroussel() {
+	img.src = basePath + slides[index].image 
+	img.alt = slides[index].tagLine 
+	txt.innerHTML = slides[index].tagLine // Utilise innerHTML pour afficher le HTML dans tagLine
+}
+
+// LES BOUTONS "EVENT LISTENER"
+///// BTN APRES 
+const boutonGauche = document.getElementById("btnGauche")
+boutonGauche.addEventListener("click", () => {
+    index = (index - 1 + slides.length) % slides.length // L'opération modulo (%) permet de "boucler" l'index. Cela signifie que si l'index > slides.length, index sera réinitialisé à 0.
+    caroussel()
+});
+/////BTN AVANT 
+const boutonDroite = document.getElementById("btnDroite")
+boutonDroite.addEventListener("click", () => {
+    index = (index + 1) % slides.length
+    caroussel()
+});
+
+//AFFICHAGE DES ELEMENTS 
 banner.appendChild(img)
+banner.appendChild(txt)
